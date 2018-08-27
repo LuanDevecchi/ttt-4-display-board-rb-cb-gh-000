fe0bc0b1a8f517e7b6d455155ed1fd943da89075
Luan Devecchi <gitluan@engineer.com>
2018-08-27 21:27:42 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,14 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 board = [" "," "," "," "," "," "," "," "," "]
+
+def makeplay(x)
+  return " #{x} "
+
+
 def display_board(state)
   separator = '|'
-  blank_space = '   '
+  blank_space = " #{}"
   dash = '-----------'
   puts blank_space + separator + blank_space + separator + blank_space
   puts dash
