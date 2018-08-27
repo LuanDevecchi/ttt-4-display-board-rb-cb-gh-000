8af7f6ec867120f4f630ce7282d47d3156957023
Luan Devecchi <gitluan@engineer.com>
2018-08-27 21:29:47 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -11,11 +11,11 @@ def display_board(state)
   separator = '|'
   blank_space = '   '
   dash = '-----------'
-  puts makeplay(state[0]) + separator + blank_space + separator + blank_space
+  puts makeplay(state[0]) + separator + makeplay(state[1]) + separator + makeplay(state[2]])
   puts dash
-  puts blank_space + separator + blank_space + separator + blank_space
+  puts makeplay(state[3]) + separator + makeplay(state[4]) + separator + makeplay(state[5])
   puts dash
-  puts blank_space + separator + blank_space + separator + blank_space
+  puts makeplay(state[6]) + separator + makeplay(state[7]) + separator + makeplay(state[8])
 end
 
 display_board(board)
