8dec86418d65b6fc9ea21b1d0e130f58ecb553b6
Luan Devecchi <gitluan@engineer.com>
2018-08-27 21:30:37 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,17 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def makeplay(x)
+  return " #{x} "
+end
+
+
+def display_board(state)
+  separator = '|'
+  blank_space = '   '
+  dash = '-----------'
+  puts makeplay(state[0]) + separator + makeplay(state[1]) + separator + makeplay(state[2])
+  puts dash
+  puts makeplay(state[3]) + separator + makeplay(state[4]) + separator + makeplay(state[5])
+  puts dash
+  puts makeplay(state[6]) + separator + makeplay(state[7]) + separator + makeplay(state[8])
+end
